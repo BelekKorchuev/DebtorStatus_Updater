8f3a10e69c8cd8e6a4ceed809bfb4b2828dd499f
BelekKorchuev <belek01042005@gmail.com>
2025-01-08 17:45:34 +0600

    версия 2.0

--- web_driver.py
+++ web_driver.py
@@ -29,20 +29,20 @@ def create_webdriver_with_display():
     """
     try:
         # Настройка виртуального дисплея
-        # xvfb_process = setup_virtual_display()
-        # if not xvfb_process:
-        #     raise RuntimeError("Не удалось настроить виртуальный дисплей.")
+        xvfb_process = setup_virtual_display()
+        if not xvfb_process:
+            raise RuntimeError("Не удалось настроить виртуальный дисплей.")
 
         # Настройка WebDriver
         chrome_options = Options()
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
-        # chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-extensions")
         chrome_service = Service(ChromeDriverManager().install())
 
         driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-        # driver.xvfb_process = xvfb_process  # Сохраняем процесс для последующего завершения
+        driver.xvfb_process = xvfb_process  # Сохраняем процесс для последующего завершения
         return driver
     except Exception as e:
         logger.error(f"Ошибка при создании WebDriver: {e}")
